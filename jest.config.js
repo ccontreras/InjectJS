ddd8e1dc78c5446883585fcf7f31faa2220ce49d
Cesar Contreras <ccdl15c@gmail.com>
2018-08-15 13:35:55 -0400

    Ignore lib for testing

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   bail: true,
   testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/lib/', '<rootDir>/node_modules/'],
   transform: {
     ['^.+\\.jsx?$']: 'babel-jest'
   }
